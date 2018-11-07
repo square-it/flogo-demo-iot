3e0365974a343dc64b698a392564d171d18f87fb
Mathieu Debove <mathieu.debove@sqits.net>
2018-11-07 16:29:21 +0100

    Delete vendor directory

--- src/flogo-demo-iot/vendor/github.com/TIBCOSoftware/flogo-contrib/activity/aggregate/window/function.go
+++
@@ -1,8 +0,0 @@
-package window
-
-type AddSampleFunc func(current, new interface{}) interface{}
-
-type AggregateSingleFunc func(value interface{}, count int) interface{}
-
-type AggregateBlocksFunc func(block []interface{}, size int) interface{}
-
--- src/flogo-demo-iot/vendor/github.com/TIBCOSoftware/flogo-contrib/activity/aggregate/window/functions/noop.go
+++
@@ -1,5 +0,0 @@
-package functions
-
-func AggregateSingleNoopFunc(value interface{}, count int) interface{} {
-	return value
-}
