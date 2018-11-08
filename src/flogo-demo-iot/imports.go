6a90e280ef445fcb13d7cdbeeefb1160a99fcf53
Mathieu Debove <debovema@users.noreply.github.com>
2018-11-08 17:33:19 +0100

    OpenTracing listener (#3)
    * Install OpenTracing listener
    * Add OpenTracing configuration

--- src/flogo-demo-iot/imports.go
+++ src/flogo-demo-iot/imports.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/actreply"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/log"
 	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/rest"
+	_ "github.com/debovema/flogo-opentracing-listener"
 	_ "github.com/square-it/flogo-contrib-activities/command"
 	_ "github.com/square-it/flogo-contrib-activities/copyfile"
 )
